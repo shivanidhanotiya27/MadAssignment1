1d8cadd74152570f11fb9e7d74f700fd210c8fed
924119 <shivani.dhanotiya@cognizant.com>
2023-07-08 19:19:24 +0530

    Initial commit with updated code

---
+++ app/src/main/java/com/mad/assignment1/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.mad.assignment1.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mad.assignment1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.counterTextView.text = counter.toString()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.clickMeButton.setOnClickListener{
+            counter++
+            binding.counterTextView.text = counter.toString()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        counter = 0
+        binding.counterTextView.text = counter.toString()
+    }
+
+}
